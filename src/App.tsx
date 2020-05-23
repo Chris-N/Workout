9641bb6e2197378f6c42255bbda9af795568b2ad
Chris-N <sdchrisn@gmail.com>
2020-05-23 16:22:04 -0400

    Established hello world typescript react app with vscode debug enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const clickHandler = () => {
+    console.log("You clicked bruh!");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,9 +14,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>
-          Yo this your typescript app!!
-        </p>
+        <p>Yo this your typescript app!!</p>
+        <button onClick={clickHandler}>Click me!</button>
         <a
           className="App-link"
           href="https://reactjs.org"
